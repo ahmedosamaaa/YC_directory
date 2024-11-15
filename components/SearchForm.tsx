41be21e945c4dc93a6b30ee5a7edb0dfbed126de
Ahmed <ahmedosama12313@gmail.com>
2024-11-15 03:08:14 +0200

    styling hero section

---
+++ components/SearchForm.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import SearchFormReset from "./SearchFormReset";
+import { Search } from "lucide-react";
+
+const SearchForm = ({ query }: { query?: string }) => {
+    return (
+        <form action="/" className="search-form">
+            <input
+                type="text"
+                name="query"
+                defaultValue={query}
+                className="search-input"
+                placeholder="Search Startups"
+            />
+
+            <div className="flex gap-2">
+                {query && <SearchFormReset />}
+                <button type="submit" className="search-btn text-white">
+                    <Search className="size-5" />
+                </button>
+            </div>
+        </form>
+    );
+};
+
+export default SearchForm;
