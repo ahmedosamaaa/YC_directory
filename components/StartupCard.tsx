a04ab8e6b0479b15f260a51c0dfc18f139789540
Ahmed <ahmedosama12313@gmail.com>
2024-11-24 08:33:17 +0200

    Add sceleton for StartupCard Component

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -1,10 +1,11 @@
-import { formateDate } from "@/lib/utils";
+import { cn, formateDate } from "@/lib/utils";
 import { EyeIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { Button } from "./ui/button";
 import { Author, Startup } from "@/sanity.types";
+import { Skeleton } from "./ui/skeleton";
 
 export type StartupTypeCard = Omit<Startup, "author"> & { author?: Author };
 
@@ -73,5 +74,15 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
         </li>
     );
 };
+
+export const StartupCardSkeleton = () => (
+    <>
+        {[0, 1, 2, 3, 4].map((index: number) => (
+            <li key={cn("skeleton", index)}>
+                <Skeleton className="startup-card_skeleton" />
+            </li>
+        ))}
+    </>
+);
 
 export default StartupCard;
