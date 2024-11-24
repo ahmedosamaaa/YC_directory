a85fa8a2a4f04562d1a7856836dfc95dfba6948c
Ahmed <ahmedosama12313@gmail.com>
2024-11-24 08:34:28 +0200

    add profile avatar in navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,8 @@ import { auth, signIn, signOut } from "@/auth";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { BadgePlus, LogOut } from "lucide-react";
 
 const Navbar = async () => {
     const session = await auth();
@@ -14,7 +16,10 @@ const Navbar = async () => {
                 <div className="flex items-center gap-5 text-black">
                     {session?.user ? (
                         <>
-                            <Link href="/startup/create">Create</Link>
+                            <Link href="/startup/create">
+                                <span className="max-sm:hidden">Create</span>
+                                <BadgePlus className="size-6 sm:hidden" />
+                            </Link>
 
                             <form
                                 action={async () => {
@@ -22,11 +27,22 @@ const Navbar = async () => {
                                     await signOut({ redirectTo: "/" });
                                 }}
                             >
-                                <button type="submit">Logout</button>
+                                <button type="submit">
+                                    <span className="max-sm:hidden">
+                                        Logout
+                                    </span>
+                                    <LogOut className="size-6 sm:hidden text-red-500" />
+                                </button>
                             </form>
 
                             <Link href={`user/${session?.id}`}>
-                                <span>{session?.user?.name}</span>
+                                <Avatar className="size-10">
+                                    <AvatarImage
+                                        src={session?.user?.image || ""}
+                                        alt={session?.user?.name || ""}
+                                    />
+                                    <AvatarFallback>AV</AvatarFallback>
+                                </Avatar>
                             </Link>
                         </>
                     ) : (
